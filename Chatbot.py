3ca1d400d89886978517592ea27d5de8dd899218
wenjus26 <rejuste.wenoumi@epitech.eu>
2023-09-02 07:20:29 +0100

    second commit

--- Chatbot.py
+++ Chatbot.py
@@ -1,28 +1,37 @@
 import openai
 import streamlit as st
 
-with st.sidebar:
-    openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
-    "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
-    "[View the source code](https://github.com/streamlit/llm-examples/blob/main/Chatbot.py)"
-    "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
+# Mettez à jour votre clé API OpenAI ici
+openai_api_key = "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"
 
+# Configurez la clé API OpenAI
+openai.api_key = openai_api_key
+
+# Définissez le titre de votre application Streamlit
 st.title("💬 Chatbot")
+
+# Initialisez la liste des messages si elle n'existe pas déjà
 if "messages" not in st.session_state:
-    st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
+    st.session_state.messages = [{"role": "assistant", "content": "Comment puis-je vous aider?"}]
 
+# Affichez les messages existants
 for msg in st.session_state.messages:
-    st.chat_message(msg["role"]).write(msg["content"])
+    st.text(f"{msg['role']}: {msg['content']}")
 
-if prompt := st.chat_input():
-    if not openai_api_key:
-        st.info("Please add your OpenAI API key to continue.")
-        st.stop()
+# Ajoutez une entrée pour l'utilisateur
+user_input = st.text_input("Votre message:")
+if user_input:
+    # Ajoutez le message de l'utilisateur à la liste
+    st.session_state.messages.append({"role": "user", "content": user_input})
 
-    openai.api_key = openai_api_key
-    st.session_state.messages.append({"role": "user", "content": prompt})
-    st.chat_message("user").write(prompt)
+    # Obtenez la réponse du chatbot
     response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
-    msg = response.choices[0].message
-    st.session_state.messages.append(msg)
-    st.chat_message("assistant").write(msg.content)
+    
+    # Récupérez la réponse du chatbot
+    bot_message = response.choices[0].message["content"]
+    
+    # Ajoutez la réponse du chatbot à la liste
+    st.session_state.messages.append({"role": "assistant", "content": bot_message})
+    
+    # Affichez la réponse du chatbot
+    st.text(f"Assistant: {bot_message}")
